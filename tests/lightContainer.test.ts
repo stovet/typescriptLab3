2d5d991f4ad9c8730c36f087a1243fb4235b2116
stovet <steven.topalian@gmail.com>
2021-12-19 09:46:11 -0500

    tests for lightContainer

--- tests/lightContainer.test.ts
+++ tests/lightContainer.test.ts
@@ -5,5 +5,19 @@ describe("testing lightContainer", function(){
         const container = new LightContainer("Tokyo", 20000);
         expect(container.destination).toEqual("Tokyo");
         expect(container.cargoWeight).toEqual(20000);
-    })
+    });
+    test("cargoWeight defaults to 0 when 2nd param omitted", function(){
+        const container = new LightContainer("China");
+        expect(container.cargoWeight).toEqual(0);
+    });
+    test("getGrossWeight returns cargoWeight", function(){
+        const container = new LightContainer("China", 25000);
+        let cargoWeight = container.getGrossWeight();
+        expect(cargoWeight).toEqual(25000);
+    });
+    test("getGrossWeight returns cargoWeight", function(){
+        const container = new LightContainer("Guam", 10000);
+        let cargoWeight = container.getGrossWeight();
+        expect(cargoWeight).toEqual(10000);
+    });
 })
